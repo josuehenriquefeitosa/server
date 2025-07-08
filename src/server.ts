0fc46bdb78a44d04430447f50ee395e204eef776
Josué Feitosa <josuehsfeitosa@gmail.com>
2025-07-08 17:02:25 -0300

    feat: add rooms listining

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,9 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod';
-import { sql } from './db/connection.ts';
 
 import { env } from './env.ts';
+import { getRoomsRoute } from './http/routes/get-rooms.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -21,5 +21,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.get('/health', () => {
   return 'OK';
 });
+
+app.register(getRoomsRoute);
 
 app.listen({ port: env.PORT });
