d25f2e8e59f07a5d5ade6f2b844f2096b4103e7a
Josué Feitosa <josuehsfeitosa@gmail.com>
2025-07-09 17:02:22 -0300

    feat: add create question, create room and get room questions

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -12,7 +12,10 @@ await seed(db, schema).refine( f => {
         name: f.companyName(),
         description: f.loremIpsum(),
       },
-    }
+    },
+    questions: {
+      count:20,
+    },
   }
 })
 
